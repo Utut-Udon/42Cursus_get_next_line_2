0cd3bb956490eebcafe1920b12aa794260cce90a
hfujita <c07014855@gmail.com>
2024-06-11 21:57:16 +0900

    francinette -s (NULL Check)

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: fujitaharuki <fujitaharuki@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/11 01:35:56 by fujitaharuk       #+#    #+#             */
-/*   Updated: 2024/06/11 03:16:19 by fujitaharuk      ###   ########.fr       */
+/*   Updated: 2024/06/11 21:23:35 by fujitaharuk      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,9 +37,7 @@ char	*read_line(int fd, char *buf, char **backup)
 	char	*tmp;
 
 	rd_size = read(fd, buf, BUFFER_SIZE);
-	if (rd_size == -1)
-		return (NULL);
-	if (rd_size == 0 && !*backup)
+	if (rd_size == -1 || (rd_size == 0 && !*backup))
 		return (NULL);
 	while (rd_size > 0)
 	{
@@ -58,28 +56,28 @@ char	*read_line(int fd, char *buf, char **backup)
 	return (ft_strdup(*backup));
 }
 
-int	get_backup(char *line, char **backup)
+char	*get_backup_line(char *line, char **backup)
 {
 	size_t	i;
 	char	*new_backup;
 
 	i = 0;
 	if (!line)
-		return (1);
+		return (NULL);
 	while (line[i] != '\0' && line[i] != '\n')
 		i++;
 	if (line[i] == '\0' || line[i + 1] == '\0')
-		return (1);
+		return (line);
 	new_backup = ft_substr(line, i + 1, ft_strlen(line) - i);
-	free(*backup);
 	if (!new_backup)
 	{
-		free(line);
-		return (1);
+		if (line != NULL)
+			free(line);
+		return (NULL);
 	}
 	line[i + 1] = '\0';
 	*backup = new_backup;
-	return (0);
+	return (line);
 }
 
 char	*get_next_line(int fd)
@@ -92,14 +90,10 @@ char	*get_next_line(int fd)
 		return (NULL);
 	buf = malloc(BUFFER_SIZE + 1);
 	line = read_line(fd, buf, &backup);
+	free(backup);
+	backup = NULL;
 	free(buf);
-	if (get_backup(line, &backup))
-	{
-		if (backup)
-		{
-			free(backup);
-			backup = NULL;
-		}
-	}
+	buf = NULL;
+	line = get_backup_line(line, &backup);
 	return (line);
 }
--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -6,7 +6,7 @@
 /*   By: fujitaharuki <fujitaharuki@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/11 03:06:55 by fujitaharuk       #+#    #+#             */
-/*   Updated: 2024/06/11 03:08:08 by fujitaharuk      ###   ########.fr       */
+/*   Updated: 2024/06/11 21:27:50 by fujitaharuk      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,9 +37,7 @@ char	*read_line(int fd, char *buf, char **backup)
 	char	*tmp;
 
 	rd_size = read(fd, buf, BUFFER_SIZE);
-	if (rd_size == -1)
-		return (NULL);
-	if (rd_size == 0 && !*backup)
+	if (rd_size == -1 || (rd_size == 0 && !*backup))
 		return (NULL);
 	while (rd_size > 0)
 	{
@@ -58,28 +56,28 @@ char	*read_line(int fd, char *buf, char **backup)
 	return (ft_strdup(*backup));
 }
 
-int	get_idx(char *line, char **backup)
+char	*get_backup_line(char *line, char **backup)
 {
 	size_t	i;
 	char	*new_backup;
 
 	i = 0;
 	if (!line)
-		return (-1);
+		return (NULL);
 	while (line[i] != '\0' && line[i] != '\n')
 		i++;
 	if (line[i] == '\0' || line[i + 1] == '\0')
-		return (-1);
+		return (line);
 	new_backup = ft_substr(line, i + 1, ft_strlen(line) - i);
 	if (!new_backup)
 	{
-		free(line);
-		return (-1);
+		if (line != NULL)
+			free(line);
+		return (NULL);
 	}
 	line[i + 1] = '\0';
-	free(*backup);
 	*backup = new_backup;
-	return (0);
+	return (line);
 }
 
 char	*get_next_line(int fd)
@@ -87,21 +85,15 @@ char	*get_next_line(int fd)
 	static char	*backup[OPEN_MAX];
 	char		*line;
 	char		*buf;
-	int			i;
 
 	if (BUFFER_SIZE <= 0 || fd < 0 || OPEN_MAX < fd)
 		return (NULL);
 	buf = malloc(BUFFER_SIZE + 1);
 	line = read_line(fd, buf, &backup[fd]);
+	free(backup[fd]);
+	backup[fd] = NULL;
 	free(buf);
-	i = get_idx(line, &backup[fd]);
-	if (i == -1)
-	{
-		if (backup[fd])
-		{
-			free(backup[fd]);
-			backup[fd] = NULL;
-		}
-	}
+	buf = NULL;
+	line = get_backup_line(line, &backup[fd]);
 	return (line);
 }
